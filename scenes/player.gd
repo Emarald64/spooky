36dc6ee35e9c1f7e41838b81b8d85bf4c89de817
Alex Giller <alexgiller@proton.me>
2025-10-28 22:06:07 -0400

    added player controller

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,25 +1,80 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const ACCEL = 1000
+const JUMP_VELOCITY = -375
+const dampening:=0.15
+#const maxSpeed:=1000
+
+var hasjumped:=false
+var started_timer:=false
+var deathCount:=0
+var jumpTime:=0.0
+const maxJumpTime=0.125
+const spriteXScale:=0.33
+
+var animating:=false
 
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("reset") and not animating:
+		respawn()
+	
 	# Add the gravity.
 	if not is_on_floor():
+		#if $AnimatedSprite2D.frame==0 or jumpTime<maxJumpTime:$AnimatedSprite2D.frame=2
 		velocity += get_gravity() * delta
-
+		if not started_timer:
+			$coyoteTimer.start()
+			started_timer=true
+	else:
+		#$AnimatedSprite2D.frame=0
+		if velocity.x!=0:
+			$AnimatedSprite2D.play()
+			$AnimatedSprite2D.speed_scale=absf(velocity.x/300)
+		else:
+			$AnimatedSprite2D.stop()
+			$AnimatedSprite2D.frame=0
+		hasjumped=false
+		started_timer=false
+		$coyoteTimer.stop()
+		
+	#if not animating and$Collect.has_overlapping_areas() and $Collect.get_overlapping_areas()[0].has_meta("animation"):
+		#$"Interact Prompt".show()
+		#if Input.is_action_just_pressed("jump"):
+			#get_node("../AnimationPlayer").play($Collect.get_overlapping_areas()[0].get_meta("animation"))
+	#else:$"Interact Prompt".hide()
+			
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if not animating and ((Input.is_action_pressed("jump") and jumpTime<0.1) or (Input.is_action_just_pressed("jump") and (not $coyoteTimer.is_stopped() or is_on_floor()))):
+		if not hasjumped:
+			$"Jump Sound".play()
+			#$AnimatedSprite2D.frame=1
+		$coyoteTimer.stop()
+		if not hasjumped:
+			jumpTime=0.0
+		var jump=JUMP_VELOCITY*((1.0 if not hasjumped and Input.is_action_just_pressed("jump") else 0.0) + (delta*10) if jumpTime+delta<maxJumpTime else (maxJumpTime-jumpTime)*10)
+		velocity.y=max(velocity.y+jump,-700)
+		jumpTime+=delta
+
+		hasjumped=true
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if not animating:
+		#if not is_on_floor():velocity.y+=Input.get_axis("move_up", "move_down")*200*delta
+		var direction := Input.get_axis("move_left", "move_right")
+		if direction:
+			$AnimatedSprite2D.scale.x=spriteXScale*signf(direction)
+			velocity.x+=direction*ACCEL*delta*(1 if signf(velocity.x)==signf(direction) else 2)
+		else:
+			velocity.x-=minf(ACCEL*2*delta,abs(velocity.x))*signf(velocity.x)
+		velocity.x*=pow(dampening,delta)
+	else:velocity.x=0
 
+	$Label.text=str(velocity)
 	move_and_slide()
+
+func respawn()->void:
+	pass
